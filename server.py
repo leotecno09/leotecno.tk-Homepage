87e194c00735165a57e9dc25c8b173c6f68bb643
leotecno09 <leotecno09@gmail.com>
2023-12-03 13:39:24 +0100

    some final adjustments... working on popups

--- server.py
+++ server.py
@@ -31,13 +31,6 @@ app.config['MAIL_PASSWORD'] = 'xxxxxxxxxxxxxxxxxxxxxxxxx'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
-#app.config['MAIL_SERVER'] = 'smtp-relay.gmail.com'
-#app.config['MAIL_PORT'] = 587
-#app.config['MAIL_USERNAME'] = 'leochat994@gmail.com'
-#app.config['MAIL_PASSWORD'] = 'Leonardo*09'
-#app.config['MAIL_USE_TLS'] = True
-#app.config['MAIL_USE_SSL'] = False
-
 mail = Mail(app)
 
 # LOGIN MANAGER
